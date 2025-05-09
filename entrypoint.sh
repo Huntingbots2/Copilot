5aab2a74c3fa1b7d217ab5f2997720a880566b78
Huntingbots2 <tgh191119@gmail.com>
2025-05-09 23:42:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,12 @@
-entrypoint.sh
+#!/bin/bash
+
+DATABASE_URL="postgresql://tghbot_owner:npg_jae9mlh4kOMN@ep-shy-feather-a4hofch2-pooler.us-east-1.aws.neon.tech/tghbot?sslmode=require"  # Update with your actual database URL
+
+# Ensure the database URL is set
+if [ -z "$DATABASE_URL" ]; then
+    echo "Error: Database URL is not set"
+    exit 1
+fi
+
+# Execute the main command
+exec "$@"
